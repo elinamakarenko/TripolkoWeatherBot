d215ccd2ca4d99505e347fa7cbdab26db57ed28e
elinamakarenko <68436291+elinamakarenko@users.noreply.github.com>
2020-08-18 00:39:48 +0300

    fourCom

--- TripolkoWeatherBot/src/main/java/Main.java
+++ TripolkoWeatherBot/src/main/java/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		ApiContextInitializer.init();
 		TelegramBotsApi botsApi = new TelegramBotsApi();
+		
 		try {
 			botsApi.registerBot(new Bot());
 		} catch (Exception ex) {
